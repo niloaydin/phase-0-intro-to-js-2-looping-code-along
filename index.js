99e2e2990527108d4dfd748a27d25236dd97c272
niloaydin <nilay.aydn97@gmail.com>
2022-04-25 02:11:23 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arr, name) {
+    const newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${name} gift!`)
+    }
+    return newArr
+}
+
+function countDown(arg) {
+    while (arg >= 0) {
+        console.log(arg)
+        arg--
+    }
+}
